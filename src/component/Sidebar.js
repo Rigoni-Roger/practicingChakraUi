79feced31a0d1de7018715b98ba2bdec0f4b4da2
Roger Rigoni <rogerrigoni.a@gmail.com>
2023-01-27 17:59:22 -0300

    Created Sidebar component with Navlinks

---
+++ src/component/Sidebar.js
@@ -0,0 +1,28 @@
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import { List, ListIcon, ListItem } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+
+export const Sidebar = () => {
+  return (
+    <List color="white" fontSize="1.2em" spacing={2}>
+      <ListItem>
+        <NavLink to="/">
+          <ListIcon as={CalendarIcon} color="white" />
+          Dashboard
+        </NavLink>
+      </ListItem>
+      <ListItem>
+        <NavLink to="create">
+          <ListIcon as={EditIcon} color="white" />
+          Create
+        </NavLink>
+      </ListItem>
+      <ListItem>
+        <NavLink to="profile">
+          <ListIcon as={AtSignIcon} color="white" />
+          Profile
+        </NavLink>
+      </ListItem>
+    </List>
+  );
+};
--- src/layout/RootLayout.js
+++ src/layout/RootLayout.js
@@ -1,12 +1,14 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../component/Navbar";
+import { Sidebar } from "../component/Sidebar";
 
 export const RootLayout = () => {
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
-      <GridItem as="aside" p={{base: "20px", lg: "30px"}} colSpan={{base: 6, lg: 2, xl: 1}} minHeight={{lg: "100hv"}}  bg="purple.400">
-        <span>SIDE BAR</span>
+      <GridItem as="aside" p={{base: "20px", lg: "30px"}} colSpan={{base: 6, lg: 2, xl: 1}} minHeight={{lg: "100vh"}}  bg="purple.400">
+        {/* <span>SIDE BAR</span> */}
+        <Sidebar />
       </GridItem>
       <GridItem  as="main" colSpan={{base: 6, lg: 4, xl: 5}} p="40px">
         <Navbar />
